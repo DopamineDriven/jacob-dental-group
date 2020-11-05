f964e0f59a11788a0112a6d72c5665e0d730a9e8
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-11-05 01:51:47 -0600

    template established

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# jacob-dental-group
-- use the dev branch
+# Jacob Dental Group
+
+## Use the dev branch
+
+## to switch from main to
+
+```git
+git add . && git commit -m "switching to dev" && git checkout -b dev
+```
+
+### Generate a random secret
+
+- open the terminal, type "node", hit enter
+- next, input the following:
+
+```git
+require('crypto').randomBytes(64).toString('hex')
+```
---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+	DocumentInitialProps
+} from 'next/document';
+
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps: DocumentInitialProps = await Document.getInitialProps(
+			ctx
+		);
+		return { ...initialProps };
+	}
+	render() {
+		return (
+			<Html lang='en-US'>
+				<Head>
+					<meta charSet='utf-8' />
+					<link rel='stylesheet' href='/fonts/index.css' />
+					<link rel='stylesheet' href='https://use.typekit.net/cub6off.css' />
+				</Head>
+				<Main />
+				<NextScript />
+			</Html>
+		);
+	}
+}
